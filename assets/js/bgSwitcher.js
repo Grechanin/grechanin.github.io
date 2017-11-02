4f123f155e9e5ed15b38db87727b5e5514bb7b0d
Grechanin <grechanin@gmail.com>
2017-11-02 16:28:40 +0200

    Update bgSwitcher.js

--- assets/js/bgSwitcher.js
+++ assets/js/bgSwitcher.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   $(document).scroll(function() {
     if ($(document).scrollTop() < $(window).height()) {
-    $('#outer-background-image').css({'background-image': 'url(../img/bg01.jpg)', 'transition': '1s'});
+    $('#outer-background-image').css({'background-image': 'url(assets/img/bg01.jpg)', 'transition': '1s'});
  }
- else if ($(document).scrollTop() == $(document).height() - $(window).height()) {
-     $('#outer-background-image').css({'background-image': 'url(../img/bg04.jpg)', 'transition': '1s',});
+    else if ($(document).scrollTop() == $(document).height() - $(window).height()) {
+    $('#outer-background-image').css({'background-image': 'url(assets/img/bg04.jpg)', 'transition': '1s',});
  }
- else if ($(document).scrollTop() > $(window).height() * 2 - '200') {
-     $('#outer-background-image').css({'background-image': 'url(../img/bg03.jpg)', 'transition': '1s'});
+    else if ($(document).scrollTop() > $(window).height() * 2 - '200') {
+    $('#outer-background-image').css({'background-image': 'url(assets/img/bg03.jpg)', 'transition': '1s'});
  }
- else if ($(document).scrollTop() > $(window).height() - '200') {
-     $('#outer-background-image').css({'background-image': 'url(../img/bg02.jpg)', 'transition': '1s'});
+    else if ($(document).scrollTop() > $(window).height() - '200') {
+    $('#outer-background-image').css({'background-image': 'url(assets/img/bg02.jpg)', 'transition': '1s'});
  }
 
   });
