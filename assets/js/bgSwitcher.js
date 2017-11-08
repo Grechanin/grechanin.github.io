5e77a9ea60eb0adcde8d88e273dcfda7ffb29866
Grechanin <grechanin@gmail.com>
2017-11-08 11:05:33 +0200

    Update bgSwitcher.js

--- assets/js/bgSwitcher.js
+++ assets/js/bgSwitcher.js
@@ -1,17 +1,32 @@
 $(document).ready(function() {
-  $(document).scroll(function() {
+  	bgSwicher();
+  	glyphiconShow();
+});
+
+
+$(document).scroll(function() {
+	bgSwicher();
+	glyphiconShow();
+});
+
+function bgSwicher (){
     if ($(document).scrollTop() < $(window).height()) {
     $('#outer-background-image').css({'background-image': 'url(assets/img/bg01.jpg)', 'transition': '1s'});
  }
-    else if ($(document).scrollTop() == $(document).height() - $(window).height()) {
-    $('#outer-background-image').css({'background-image': 'url(assets/img/bg04.jpg)', 'transition': '1s',});
+ 	else if ($('#menu-item-history').attr('class') == 'active') {
+    $('#outer-background-image').css({'background-image': 'url(assets/img/bg02.jpg)', 'transition': '1s',});
  }
-    else if ($(document).scrollTop() > $(window).height() * 2 - '200') {
+ 	else if ($('#menu-item-text').attr('class') == 'active') {
     $('#outer-background-image').css({'background-image': 'url(assets/img/bg03.jpg)', 'transition': '1s'});
  }
-    else if ($(document).scrollTop() > $(window).height() - '200') {
-    $('#outer-background-image').css({'background-image': 'url(assets/img/bg02.jpg)', 'transition': '1s'});
+ 	else if ($('#menu-item-contact').attr('class') == 'active') {
+    $('#outer-background-image').css({'background-image': 'url(assets/img/bg04.jpg)', 'transition': '1s'});
  }
+};
 
-  });
-});
+function glyphiconShow(){
+	if ($(document).scrollTop() > $(window).height()){
+		$('#go-to-top').addClass('active');
+	}
+	else $('#go-to-top').removeClass('active');
+}
