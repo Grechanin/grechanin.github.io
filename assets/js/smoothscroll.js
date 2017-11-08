3edbbe4a559138e402deb214815e000917c62685
Grechanin <grechanin@gmail.com>
2017-11-08 11:12:27 +0200

    Update smoothscroll.js

--- assets/js/smoothscroll.js
+++ assets/js/smoothscroll.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
-  $("a").click(function() {
+  $("a, span").click(function() {
     var elementClick = $(this).attr("href")
     var destination = $(elementClick).offset().top;
     jQuery("html:not(:animated),body:not(:animated)").animate({
       scrollTop: destination
-    }, 800);
+    }, 1000);
     return false;
   });
 });
